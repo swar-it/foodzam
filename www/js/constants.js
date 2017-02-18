78b5c69bdd87494a6d79e5400231cb54387dfeaf
Swarit Agarwal <agarwalswarit@gmail.com>
2017-02-18 22:10:10 +0800

    login and registration WIP

---
+++ www/js/constants.js
@@ -0,0 +1,11 @@
+angular.module('starter')
+ 
+.constant('AUTH_EVENTS', {
+  notAuthenticated: 'auth-not-authenticated'
+})
+ 
+.constant('API_ENDPOINT', {
+  // url: 'https://ionic-nodejs.azurewebsites.net'
+  url: 'http://localhost'
+  //  For a simulator use: url: 'http://127.0.0.1:8080/api'
+});
